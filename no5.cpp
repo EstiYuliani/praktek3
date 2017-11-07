6a8bdeb3353abf3f058616f9d9e5b19a60bd43d1
EstiYuliani <32305158+EstiYuliani@users.noreply.github.com>
2017-11-07 23:23:04 +0800

    membuat input & output

--- no5.cpp
+++ no5.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main(){
 	double berat, tinggi  (2.5);
-
+	
+	cout<<"input tinggi badan(cm)\t:";
+	cin>>tinggi;
+	cout<<"input berat badan(kg)\t:";
+	cin>>berat;
+	
+	
 }
