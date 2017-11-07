ac33983e688b50fe5200517754f697453b57b491
EstiYuliani <32305158+EstiYuliani@users.noreply.github.com>
2017-11-07 22:45:21 +0800

    revisi kata kata dan selesai

--- no2.cpp
+++ no2.cpp
@@ -6,37 +6,38 @@ int main(){
 	char pilihan,yt;
 	double ls,lp,p,l,a,t(2.5);
 	
-	cout <<"========================\n";
+	cout <<"*************************\n";
 	cout <<"        pilihan\n";
 	cout <<"P = Luas persegi panjang\n";
 	cout <<"S = Luas segitiga\n";
-	cout <<"========================\n";
-	cout <<"silahkan masukkan pilihan:";
+	cout <<"*************************\n";
+	cout <<"silahkan input pilihan:";
 	cin >>pilihan;
 	
 	if (pilihan=='p'|| pilihan=='P')
 	{
-		cout<<"masukkan panjang\t:";
+		cout<<"input panjang\t:";
 		cin >>p;
-		cout<<"masukkan lebar\t\t:";
+		cout<<"input lebar\t\t:";
 		cin >>l;
 		lp=p*l;
 		cout<<"Luas persegi adalah\t:"<<lp<<"\n";
 	}
 	else if 	(pilihan=='s'|| pilihan=='S')
 	{
-		cout<<"masukkan alas\t\t:";
+		cout<<"input alas\t\t:";
 		cin >>a;
-		cout<<"masukkan tinggi\t\t:";
+		cout<<"input tinggi\t\t:";
 		cin >> t;
 		ls=0.5*(a*t);
 		cout<<"luas segitiga adalah\t:"<<ls<<"\n";
 	}
 	else
-	cout<<"kode yang anda masukkan salah\t"<<"\n";
+	cout<<"kode yang anda input salah\t"<<"\n";
 	
 	
-	
+		selesai:
+	cout<<"Terima Kasih";
 	
 	
 }
