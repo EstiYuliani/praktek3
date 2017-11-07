c036a227bd44f8d1dd46027ea8ffba2f2e129902
EstiYuliani <32305158+EstiYuliani@users.noreply.github.com>
2017-11-07 22:38:21 +0800

    membuat output dan input

--- no2.cpp
+++ no2.cpp
@@ -5,5 +5,15 @@ using namespace std;
 int main(){
 	char pilihan,yt;
 	double ls,lp,p,l,a,t(2.5);
-
+	
+	cout <<"========================\n";
+	cout <<"        pilihan\n";
+	cout <<"P = Luas persegi panjang\n";
+	cout <<"S = Luas segitiga\n";
+	cout <<"========================\n";
+	cout <<"silahkan masukkan pilihan:";
+	cin >>pilihan;
+	
+	
+	
 }
