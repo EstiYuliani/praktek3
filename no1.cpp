3527606eb12805791e2dce21771b21c41c7a5d3f
EstiYuliani <32305158+EstiYuliani@users.noreply.github.com>
2017-11-07 22:27:14 +0800

    membuat output dan input

--- no1.cpp
+++ no1.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int main(){
 	int x,y,h;
 	char yt;
+	awal:
+	cout<<"program pengurangan tanpa angka negatif\n";
+	cout<<"masukkan x\t:";
+	cin>>x;
+	cout<<"masukan y\t:";
+	cin>>y;
 	
 	
 }
